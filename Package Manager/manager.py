80637b5a1445e21bf16824ea6c7000f97fb0542a
Proksima Erboga <proxima_erboga@windowslive.com>
2022-12-23 06:18:20 +0300

    terminal version manager.py file

--- Package Manager/manager.py
+++ Package Manager/manager.py
@@ -2,76 +2,81 @@ import subprocess
 import os
 
 class PackageManager(object):
-    def __init__(self): #, pkg, version = None
-        self.pkgs = []
+    def __init__(self, choice):
+        self.choice = choice
+        if self.choice == 1:
+            self.installer()
+        elif self.choice == 2:
+            self.displayer()
+        elif self.choice == 3:
+            self.uninstaller()
+        elif self.choice == 4:
+            self.updater()
+        elif self.choice == 5:
+            pass
+        else:
+            print("Undefined choice!")
 
-    def pkg_list(self):
+        self.do = "None"
+        self.name = None
+
+    def re_do(self):
+        answer = int(input(f"\nWant to {self.do} another package?\n1. Yes\n2. No\n"))
+        return answer - 1
+
+    def installer(self):
+        self.do = "install"
+
+        flag = 0
+        while flag == 0:
+            self.name = input(f"Package name to {self.do}: ")
+            package_version = input("Package version (enter 0 for the latest available version): ")
+        
+            if package_version == "0":
+                subprocess.call("python -m pip install --upgrade " + self.name, shell = True)
+                flag = self.re_do()
+
+            else:
+                subprocess.call("python -m pip install --upgrade " + self.name + "==" + package_version, shell = True)
+                flag = self.re_do()
+    
+        input("Installation(s) are completed.")
+
+    def uninstaller(self):
+        self.do = "uninstall"        
+
+        flag = 0
+        while flag == 0:
+            self.name = input(f"Package name to {self.do}: ")
+
+            subprocess.call("python -m pip uninstall " + self.name, shell = True)
+            flag = self.re_do()
+
+    def displayer(self):
+        self.do = "display"
+        
+        flag = 0
+        while flag == 0:
+            self.name = input(f"Package name to {self.do}: ")
+            
+            subprocess.call("python -m pip show " + self.name, shell = True)
+            flag = self.re_do()
+
+    def updater(self):
+        self.do = "update"
         os.chdir(".")
 
-        subprocess.call("pip list > packages.txt", shell = True)
+        subprocess.call("pip list > requirements.txt", shell = True)
 
-        with open("packages.txt") as f:
+        with open("requirements.txt") as f:
+            package_list = []
             for i, line in enumerate(f):
                 if i == 0 or i == 1:
                     pass
                 else:
-                    self.pkgs.append(line.split(' ')[0])
+                    package_list.append(line.split(' ')[0])
 
-        os.remove("packages.txt")
+        os.remove("requirements.txt")
 
-    def installer(self, package, version = None):
-        try:
-            if version == None: # update single package or install latest version
-                subprocess.call("python -m pip install --upgrade " + package, shell = True)
-
-            else: # install selected version
-                subprocess.call("python -m pip install --upgrade " + package + "==" + version, shell = True)
-
-            return True, 1
-
-        except Exception as e:
-            return (False, e)
-
-    def uninstaller(self, package):
-        try:
-            subprocess.call("python -m pip uninstall " + package, shell = True)
-            return True, 1
-
-        except Exception as e:
-            return False, e
-
-    def searcher(self, src):
-        self.pkg_list()
-
-        try:            
-            possible_pkgs = []
-            for pkg in self.pkgs:
-                if src in pkg:
-                    possible_pkgs.append(pkg)
-
-            return True, possible_pkgs
-        
-        except Exception as e:
-            return False, e
-
-    def pkg_info(self, package):
-        try:
-            subprocess.call("python -m pip show " + package, shell = True)            
-            return True, 1
-
-        except Exception as e:
-            return False, e
-
-    def updater(self): # all packages
-        try:
-            self.pkg_list()
-            for pkg in self.pkgs:
-                subprocess.call("pip install --upgrade " + pkg, shell = True)
-            return True, 1
-
-        except Exception as e:
-            return False, e
-
-    
-
-
+        for pkg in package_list:
+            subprocess.call("pip install --upgrade " + pkg, shell = True)
