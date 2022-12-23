b75814dc0c7aa5e555627b92933559a00a401621
Proksima Erboga <proxima_erboga@windowslive.com>
2022-12-23 06:19:32 +0300

    terminal version files

--- Package Manager/Package_Manager.py
+++ Package Manager/Package_Manager.py
@@ -1,8 +1,30 @@
 
-#from manager import PackageManager
-#from interface import *
+from manager import PackageManager
 
-#while (1):
+def exit():
+    exit_choice = input("\nWant to exit?\n1. Yes\n2. No\n")
+
+    if exit_choice == "1" or exit_choice.lower() == "yes":
+        input("Thank you for using my program.\nPress Enter to exit.")
+        return 1
     
-#    manager_handler = PackageManager(read_data)
-#    del manager_handler
+    elif exit_choice == "2" or exit_choice.lower() == "no":
+        return 0
+    
+    else:
+        input("Undefined selection.\nProgram will be terminated.")
+        return 1
+
+while (1):
+    selection = int(input("1. Install package(s)\n2. Show package info\n3. Uninstall package(s)\n4. Update all packages\n5. Exit\n"))
+
+    manager_handler = PackageManager(selection)
+
+    del manager_handler
+
+    if selection == 5:
+        exit_handler = exit()
+        if exit_handler:
+            break
+        else:
+            continue
